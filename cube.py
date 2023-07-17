b7b7d14975dc2df9c1029253171ecc77789f3262
Toby Killen <itoby24@gmail.com>
2023-07-17 01:36:59 +0100

    Slowly getting there

--- cube.py
+++ cube.py
@@ -1,7 +1,8 @@
 import random
-
+import pandas
 class Cube:
     def __init__(self, front, back, left, right, top, bottom):
+        # Front, back, left, right, top, bottom
         self.front = front
         self.back = back
         self.left = left
@@ -9,25 +10,42 @@ class Cube:
         self.top = top
         self.bottom = bottom
 
+    def __str__(self) -> str:
+        return f"Cube Front: {self.front}, Cube Back: {self.back}, Cube Left: {self.left}, Cube Right: {self.right}, Cube Top: {self.top}, Cube Bottom: {self.bottom}"
 
-    # Func to rotate cube in any given direction
-    def rotate_cube(self, direction):
-        if direction == "right":
-            self.front, self.right, self.back, self.left = self.right, self.back, self.left, self.front
-        elif direction == "left":
-            self.front, self.right, self.back, self.left = self.left, self.front, self.right, self.back
-        elif direction == "up":
+    def rotate_cube(self, axis):
+        if axis == "random":
+            axis = random.choice(["x", "y", "z"])
+        if axis == "x":
             self.front, self.top, self.back, self.bottom = self.bottom, self.front, self.top, self.back
-        elif direction == "down":
-            self.front, self.top, self.back, self.bottom = self.top, self.back, self.bottom, self.front
-        elif direction == "front":
-            self.top, self.right, self.bottom, self.left = self.left, self.top, self.right, self.bottom
-        elif direction == "back":
-            self.top, self.right, self.bottom, self.left = self.right, self.bottom, self.left, self.top
-        elif direction == "random":
-            # Randomly choose a direction to rotate cube
-            directions = ["right", "left", "up", "down", "front", "back"]
-            random_direction = random.choice(directions)
-            self.rotate_cube(random_direction)
+        elif axis == "y":
+            self.front, self.left, self.back, self.right = self.right, self.front, self.left, self.back
+        elif axis == "z":
+            self.top, self.left, self.bottom, self.right = self.right, self.top, self.left, self.bottom
         else:
-            print("Invalid direction")
+            raise ValueError(f"Axis must be 'x', 'y', or 'z', not {axis}")
+        
+    def get_orientation(self):
+        return pandas.Series([self.front, self.back, self.left, self.right, self.top, self.bottom], index=["Front", "Back", "Left", "Right", "Top", "Bottom"])
+        
+    def list_all_possible_orientations_per_cube(self):
+        all_possible_orientations = []
+        for _ in range(4):
+            for _ in range(4):
+                for _ in range(4):
+                    # Front, back, left, right, top, bottom
+                    possible_orientation = Cube(self.front, self.back, self.left, self.right, self.top, self.bottom)
+                    all_possible_orientations.append(possible_orientation)
+                    self.rotate_cube("z")
+                self.rotate_cube("y")
+            self.rotate_cube("x")
+        return all_possible_orientations
+
+    def set_orientation(self, orientation):
+        self.front = orientation.front
+        self.back = orientation.back
+        self.left = orientation.left
+        self.right = orientation.right
+        self.top = orientation.top
+        self.bottom = orientation.bottom
+
--- solve.py
+++ solve.py
@@ -1,99 +1,100 @@
 import random
 import logging
-
-class Cube:
-    def __init__(self, front, back, left, right, top, bottom):
-        self.front = front
-        self.back = back
-        self.left = left
-        self.right = right
-        self.top = top
-        self.bottom = bottom
-
-
-    # Func to rotate cube in any given direction
-    def rotate_cube(self, axis):
-        if axis == "random":
-            axis = random.choice(["x", "y", "z"])
-        if axis == "x":
-            self.front, self.top, self.back, self.bottom = self.bottom, self.front, self.top, self.back
-        elif axis == "y":
-            self.front, self.left, self.back, self.right = self.right, self.front, self.left, self.back
-        elif axis == "z":
-            self.top, self.left, self.bottom, self.right = self.right, self.top, self.left, self.bottom
-        else:
-            raise ValueError(f"Axis must be 'x', 'y', or 'z', not {axis}")
-
-
-
-    def does_cube_match(self, TestCube1, TestCube2):
-        if TestCube1.front == TestCube2.front:
-            if TestCube1.back == TestCube2.back:
-                if TestCube1.left == TestCube2.left:
-                    if TestCube1.right == TestCube2.right:
-                        if TestCube1.top == TestCube2.top:
-                            if TestCube1.bottom == TestCube2.bottom:
-                                return True
-                            else:
-                                return False
-                        else:
-                            return False
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return False
-
-
-
-
-
-
+import time
+from cube import Cube
+import pandas as pd
 class SolvePuzzle:
     def __init__(self) -> None:
-        # Front, back, left, right, top, bottom
+
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-        Cube1 = Cube("yellow", "green", "red", "blue", "green", "red")
-        Cube2 = Cube("red", "yellow", "yellow", "blue", "green", "red")
+        # Front, back, left, right, top, bottom
+        Cube1 = Cube("yellow", "yellow", "red", "blue", "yellow", "green")
+        Cube2 = Cube("yellow", "green", "red", "green", "red", "blue")
         Cube3 = Cube("blue", "yellow", "green", "green", "red", "blue")
-        Cube4 = Cube("red", "blue", "yellow", "yellow", "yellow", "green")
+        Cube4 = Cube("red", "green", "blue", "yellow", "yellow", "red")
+        
 
-        is_puzzle_solved = False
-        while not is_puzzle_solved:
+        # Itterate through all possible orientations of each cube and check if they are considered a solution to the puzzle
+        time_now = time.time()
+        All_Possible_Orientations_Cube_1 = Cube1.list_all_possible_orientations_per_cube()
+        All_Possible_Orientations_Cube_2 = Cube2.list_all_possible_orientations_per_cube()
+        All_Possible_Orientations_Cube_3 = Cube3.list_all_possible_orientations_per_cube()
+        All_Possible_Orientations_Cube_4 = Cube4.list_all_possible_orientations_per_cube()
+
+        for orientation1 in All_Possible_Orientations_Cube_1:
+            for orientation2 in All_Possible_Orientations_Cube_2:
+                for orientation3 in All_Possible_Orientations_Cube_3:
+                    for orientation4 in All_Possible_Orientations_Cube_4:
+                        # Set the orientations of the cubes
+                        Cube1.set_orientation(orientation1)
+                        Cube2.set_orientation(orientation2)
+                        Cube3.set_orientation(orientation3)
+                        Cube4.set_orientation(orientation4)
+                        # Check if the current orientations solve the puzzle
+                        if self.check_solution(Cube1, Cube2, Cube3, Cube4):
+                            all_possible_solutions = []
+
+
+                            possible_solution = {
+                                "Cube 1": Cube1.get_orientation(),
+                                "Cube 2": Cube2.get_orientation(),
+                                "Cube 3": Cube3.get_orientation(),
+                                "Cube 4": Cube4.get_orientation()
+                            }
+
+                            if possible_solution not in all_possible_solutions:
+                                all_possible_solutions.append(possible_solution)
+
+                                print("___________________________________________________________")
+                                print("Solution found!")
+                                print("Cube 1:", Cube1.get_orientation())
+                                print("Cube 2:", Cube2.get_orientation())
+                                print("Cube 3:", Cube3.get_orientation())
+                                print("Cube 4:", Cube4.get_orientation())
+                                print("___________________________________________________________")
+                                break  # Exit the loop if a solution is found
+                        else:
+                            logging.debug("Not a solution")
+        
+            break
+    
+
+    def check_solution(self,  cube1, cube2, cube3, cube4):
+            count_of_unique_sides = 0
+
+            top_colors = [cube1.top, cube2.top, cube3.top, cube4.top]
+            bottom_colors = [cube1.bottom, cube2.bottom, cube3.bottom, cube4.bottom]
+            left_colors = [cube1.left, cube2.left, cube3.left, cube4.left]
+            right_colors = [cube1.right, cube2.right, cube3.right, cube4.right]
+            front_colors = [cube1.front, cube2.front, cube3.front, cube4.front]
+            back_colors = [cube1.back, cube2.back, cube3.back, cube4.back]
+
+            if len(set(top_colors)) == 4 and len(top_colors) == len(set(top_colors)):
+                count_of_unique_sides += 1
+
+            if len(set(bottom_colors)) == 4 and len(bottom_colors) == len(set(bottom_colors)):
+                count_of_unique_sides += 1
+
+            if len(set(left_colors)) == 4 and len(left_colors) == len(set(left_colors)):
+                count_of_unique_sides += 1
+
+            if len(set(right_colors)) == 4 and len(right_colors) == len(set(right_colors)):
+                count_of_unique_sides += 1
+
+            if len(set(front_colors)) == 4 and len(front_colors) == len(set(front_colors)):
+                count_of_unique_sides += 1
+
+            if len(set(back_colors)) == 4 and len(back_colors) == len(set(back_colors)):
+                count_of_unique_sides += 1
+
+
+            print("Count of unique sides:", count_of_unique_sides)
+            if count_of_unique_sides >= 4:
+                return True
             
-            logging.debug("Beginning to Solve Puzzle")
-
-            cube1_cube2_match = False
-            while not cube1_cube2_match:
-                logging.debug("Cube 1 and Cube 2 do not match - Rotate Cube 2")
-                Cube1.rotate_cube("random")
-                Cube2.rotate_cube("random")
-                cube1_cube2_match = Cube1.does_cube_match(Cube1, Cube2)
-                logging.debug(f"Cube 1 and Cube 2 match: {cube1_cube2_match}")
-                is_puzzle_solved = True
-                
-
             
-
-
-    def is_puzzle_solved(self, Cube1, Cube2, Cube3, Cube4) -> bool:
-        if Cube1.front == Cube1.back == Cube1.top == Cube1.bottom:
-            if Cube2.front == Cube2.back == Cube2.top == Cube2.bottom:
-                if Cube3.front == Cube3.back == Cube3.top == Cube3.bottom:
-                    if Cube4.front == Cube4.back == Cube4.top == Cube4.bottom:
-                        return True
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return False
+            
+        
 
 if __name__ == "__main__":
     SolvePuzzle()
