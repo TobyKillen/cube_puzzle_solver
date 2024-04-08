5f5be8fd701bfc8ef7be548ac91d568b8602611c
Toby Killen <43643479+TobyKillen@users.noreply.github.com>
2024-04-08 20:46:22 +0100

    Added Final Solution and added minor README for Public Information (#3)

--- solve.py
+++ solve.py
@@ -1,11 +1,6 @@
-import random
 import logging
 import time
 from cube import Cube
-import pandas as pd
-
-# Adding comment to create PR
-# Random comment to create PR
 class SolvePuzzle:
     def __init__(self) -> None:
 
@@ -16,85 +11,45 @@ class SolvePuzzle:
         Cube3 = Cube("blue", "yellow", "green", "green", "red", "blue")
         Cube4 = Cube("red", "green", "blue", "yellow", "yellow", "red")
         
-
-        # Itterate through all possible orientations of each cube and check if they are considered a solution to the puzzle
         time_now = time.time()
         All_Possible_Orientations_Cube_1 = Cube1.list_all_possible_orientations_per_cube()
         All_Possible_Orientations_Cube_2 = Cube2.list_all_possible_orientations_per_cube()
         All_Possible_Orientations_Cube_3 = Cube3.list_all_possible_orientations_per_cube()
         All_Possible_Orientations_Cube_4 = Cube4.list_all_possible_orientations_per_cube()
+        # Fetch all possible orientations for each cube. Using a class will retain the position of each color on each cube.
 
-        for orientation1 in All_Possible_Orientations_Cube_1:
-            for orientation2 in All_Possible_Orientations_Cube_2:
-                for orientation3 in All_Possible_Orientations_Cube_3:
-                    for orientation4 in All_Possible_Orientations_Cube_4:
-                        # Set the orientations of the cubes
-                        Cube1.set_orientation(orientation1)
-                        Cube2.set_orientation(orientation2)
-                        Cube3.set_orientation(orientation3)
-                        Cube4.set_orientation(orientation4)
-                        # Check if the current orientations solve the puzzle
-                        if self.check_solution(Cube1, Cube2, Cube3, Cube4):
-                            all_possible_solutions = []
+        for cube1 in All_Possible_Orientations_Cube_1:
+            for cube2 in All_Possible_Orientations_Cube_2:
+                for cube3 in All_Possible_Orientations_Cube_3:
+                    for cube4 in All_Possible_Orientations_Cube_4:
+                        if self.check_solution(cube1, cube2, cube3, cube4):
+                            logging.info("==================================")
+                            logging.info("Solution found!")
+                            logging.info(f"Cube 1: {cube1}")
+                            logging.info(f"Cube 2: {cube2}")
+                            logging.info(f"Cube 3: {cube3}")
+                            logging.info(f"Cube 4: {cube4}")
+                            logging.info(f"Time taken: {time.time() - time_now}")
+                            logging.info("==================================")
+                            raise SystemExit
 
+    def check_solution(self, cube1, cube2, cube3, cube4):
+        """
+        Function to validate if the sides of the cubes contain unique colors. They should not contain the same color.
+        """
+        try:
+            cube_top_colors = [cube1['top'], cube2['top'], cube3['top'], cube4['top']]
+            cube_bottom_colors = [cube1['bottom'], cube2['bottom'], cube3['bottom'], cube4['bottom']]
+            cube_front_colors = [cube1['front'], cube2['front'], cube3['front'], cube4['front']]
+            cube_back_colors = [cube1['back'], cube2['back'], cube3['back'], cube4['back']]
 
-                            possible_solution = {
-                                "Cube 1": Cube1.get_orientation(),
-                                "Cube 2": Cube2.get_orientation(),
-                                "Cube 3": Cube3.get_orientation(),
-                                "Cube 4": Cube4.get_orientation()
-                            }
-
-                            if possible_solution not in all_possible_solutions:
-                                all_possible_solutions.append(possible_solution)
-
-                                print("___________________________________________________________")
-                                print("Solution found!")
-                                print("Cube 1:", Cube1.get_orientation())
-                                print("Cube 2:", Cube2.get_orientation())
-                                print("Cube 3:", Cube3.get_orientation())
-                                print("Cube 4:", Cube4.get_orientation())
-                                print("___________________________________________________________")
-                                break  # Exit the loop if a solution is found
-                        else:
-                            logging.debug("Not a solution")
-        
-            break
-    
-
-    def check_solution(self,  cube1, cube2, cube3, cube4):
-            count_of_unique_sides = 0
-
-            top_colors = [cube1.top, cube2.top, cube3.top, cube4.top]
-            bottom_colors = [cube1.bottom, cube2.bottom, cube3.bottom, cube4.bottom]
-            left_colors = [cube1.left, cube2.left, cube3.left, cube4.left]
-            right_colors = [cube1.right, cube2.right, cube3.right, cube4.right]
-            front_colors = [cube1.front, cube2.front, cube3.front, cube4.front]
-            back_colors = [cube1.back, cube2.back, cube3.back, cube4.back]
-
-            if len(set(top_colors)) == 4 and len(top_colors) == len(set(top_colors)):
-                count_of_unique_sides += 1
-
-            if len(set(bottom_colors)) == 4 and len(bottom_colors) == len(set(bottom_colors)):
-                count_of_unique_sides += 1
-
-            if len(set(left_colors)) == 4 and len(left_colors) == len(set(left_colors)):
-                count_of_unique_sides += 1
-
-            if len(set(right_colors)) == 4 and len(right_colors) == len(set(right_colors)):
-                count_of_unique_sides += 1
-
-            if len(set(front_colors)) == 4 and len(front_colors) == len(set(front_colors)):
-                count_of_unique_sides += 1
-
-            if len(set(back_colors)) == 4 and len(back_colors) == len(set(back_colors)):
-                count_of_unique_sides += 1
-
-
-            print("Count of unique sides:", count_of_unique_sides)
-            if count_of_unique_sides >= 4:
+            if len(set(cube_top_colors)) == 4 and len(set(cube_bottom_colors)) == 4 and len(set(cube_front_colors)) == 4 and len(set(cube_back_colors)) == 4:
                 return True
-            
+            else:
+                return False
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            return False
             
             
         
